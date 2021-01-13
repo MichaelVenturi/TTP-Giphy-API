896525e6600b8fb5fec02fa409c5421ec00c0f81
Justin Goodchild <jeg6300@gmail.com>
2021-01-13 15:47:46 -0500

    Edited the text input field to automatically alter the searchTerm of the prop upon any change

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -5,7 +5,7 @@ function SearchField(props){
   return <div>
       <form id="gifLookup">
         <label><b><i>Search Term:</i></b></label>
-        <input type="text" id="searchTerm" name="searchTerm" placeholder="Try cats" onChange={(event)=>console.log(event.target.value)}>
+        <input type="text" id="searchTerm" name="searchTerm" placeholder="Try cats" onChange={(event)=>props.searchTerm = event.target.value}>
         </input>
         <button type="button" onClick={()=>console.log("Regular was clicked")}>Regular Search</button>
         <button type="button" onClick={()=>console.log("Trending was clicked")}>Trending Search</button>
