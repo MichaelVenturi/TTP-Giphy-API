5c5356cf5edb382acef09facce4688b9303571c0
Justin Goodchild <jeg6300@gmail.com>
2021-01-13 15:06:53 -0500

    The buttons and text field input now log if they were clicked or what the text input was to the console, this will be changed in the future

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,19 +1,17 @@
 import React from "react";
 
-class SearchField extends React.Component {
+function SearchField(props){
   //Is this just 3 buttons and a text input?
-  render(){
-    return <div>
-      <form>
+  return <div>
+      <form id="gifLookup">
         <label><b><i>Search Term:</i></b></label>
-        <input type="text" id="searchTerm" name="searchTerm" placeholder="Try cats">
+        <input type="text" id="searchTerm" name="searchTerm" placeholder="Try cats" onChange={(event)=>console.log(event.target.value)}>
         </input>
-        <button type="button">Regular Search</button>
-        <button type="button">Trending Search</button>
-        <button type="button">Random Search</button>
+        <button type="button" onClick={()=>console.log("Regular was clicked")}>Regular Search</button>
+        <button type="button" onClick={()=>console.log("Trending was clicked")}>Trending Search</button>
+        <button type="button" onClick={()=>console.log("Random was clicked")}>Random Search</button>
       </form>
     </div>
-  }
 }
 
 export default SearchField;
