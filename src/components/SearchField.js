0cf5267e7ffd8284ebc5d631384ff74f6045e1e7
Justin Goodchild <jeg6300@gmail.com>
2021-01-13 14:23:30 -0500

    Initial commit for searchfield component

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-function SearchField() {
-  return;
+class SearchField() extends React.Component{
+  constructor(){
+    super();
+    this.state ={
+      apiKey: "fOZyz1kQZ2FT4Nsguh8pbPSdnmVmWuvm"
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Empty for now</h1>
+      </div>
+    )
+  }
 }
 
 export default SearchField;
