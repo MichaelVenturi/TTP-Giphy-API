21872a1c0aca0b4a3f61f5489b6e54b7aee2aacb
Michael Venturi <michaelventuri58@aol.com>
2021-01-13 15:54:53 -0500

    pulled search field change

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,29 +1,40 @@
 import React from "react";
 
-<<<<<<< HEAD
 function SearchField(props) {
+  //Is this just 3 buttons and a text input?
   return (
     <div>
-      {/* <input type="text" name="searchTerm" onChange={this.handleChange}></input> */}
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-    </div>
-  );
-=======
-function SearchField(props){
-  //Is this just 3 buttons and a text input?
-  return <div>
       <form id="gifLookup">
-        <label><b><i>Search Term:</i></b></label>
-        <input type="text" id="searchTerm" name="searchTerm" placeholder="Try cats" onChange={(event)=>props.searchTerm = event.target.value}>
-        </input>
-        <button type="button" onClick={()=>console.log("Regular was clicked")}>Regular Search</button>
-        <button type="button" onClick={()=>console.log("Trending was clicked")}>Trending Search</button>
-        <button type="button" onClick={()=>console.log("Random was clicked")}>Random Search</button>
+        <label>
+          <b>
+            <i>Search Term:</i>
+          </b>
+        </label>
+        <input
+          type="text"
+          id="searchTerm"
+          name="searchTerm"
+          placeholder="Try cats"
+          onChange={(event) => (props.searchTerm = event.target.value)}
+        ></input>
+        <button
+          type="button"
+          onClick={() => console.log("Regular was clicked")}
+        >
+          Regular Search
+        </button>
+        <button
+          type="button"
+          onClick={() => console.log("Trending was clicked")}
+        >
+          Trending Search
+        </button>
+        <button type="button" onClick={() => console.log("Random was clicked")}>
+          Random Search
+        </button>
       </form>
     </div>
->>>>>>> 2a446dec48c947e65bcd9ceb667f00b28470d623
+  );
 }
 
 export default SearchField;
