6802067106bf5e00146c3253bf1395d51f73228c
Michael Venturi <michaelventuri58@aol.com>
2021-01-13 14:41:06 -0500

    setup app.js as a class, set default states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
+import reactDOM from "react-dom";
 import "./App.css";
+import GifCard from "./components/GifCard";
+import SearchField from "./components/SearchField";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
-  );
+//API Key:  fOZyz1kQZ2FT4Nsguh8pbPSdnmVmWuvm
+class App extends React.Component() {
+  constructor() {
+    super();
+
+    this.state = {
+      searchTerm: "",
+      results: [],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>hello world</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
