13d431895db51f796832c4d27dda64c48eefcdef
Justin Goodchild <jeg6300@gmail.com>
2021-01-13 14:51:54 -0500

    searchField now contains a text input, and three buttons for the different types of searches

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,19 +1,18 @@
 import React from "react";
 
-class SearchField() extends React.Component{
-  constructor(){
-    super();
-    this.state ={
-      apiKey: "fOZyz1kQZ2FT4Nsguh8pbPSdnmVmWuvm"
-    }
-  }
-
+class SearchField extends React.Component {
+  //Is this just 3 buttons and a text input?
   render(){
-    return(
-      <div>
-        <h1>Empty for now</h1>
-      </div>
-    )
+    return <div>
+      <form>
+        <label><b><i>Search Term:</i></b></label>
+        <input type="text" id="searchTerm" name="searchTerm" placeholder="Try cats">
+        </input>
+        <button type="button">Regular Search</button>
+        <button type="button">Trending Search</button>
+        <button type="button">Random Search</button>
+      </form>
+    </div>
   }
 }
 
